f70d3e26e253f541915137974a49c7e4947d52e9
fastslack <fastslack@efa60c74-61cf-4e71-b3ad-ebe38a3d3cd1>
2009-07-26 04:36:01 +0000

    update database structure
    git-svn-id: http://matware.com.ar/svn/mtwMultiple/trunk@41 efa60c74-61cf-4e71-b3ad-ebe38a3d3cd1

--- admin/sql/install.sql
+++ admin/sql/install.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `jos_mtwmultiple_sites`;
+DROP TABLE IF EXISTS `#__mtwmultiple_sites`;
 CREATE TABLE IF NOT EXISTS `jos_mtwmultiple_sites` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(255) collate utf8_unicode_ci NOT NULL,
@@ -7,6 +7,23 @@ CREATE TABLE IF NOT EXISTS `jos_mtwmultiple_sites` (
   `created_by` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `password` varchar(255) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `#__mtwmultiple_extensions`;
+CREATE TABLE IF NOT EXISTS `jos_mtwmultiple_extensions` (
+  `id` int(11) NOT NULL auto_increment,
+  `filename` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `type` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `name` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `author` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `creationDate` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `copyright` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `license` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `authorEmail` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `authorUrl` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `version` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `enable` int(11) NOT NULL default '1',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
