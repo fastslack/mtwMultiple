ece56360b7cbadd57929aae77e2e142739d5ca27
Matias Aguirre <maguirre@matware.com.ar>
2012-03-07 04:18:34 -0300

    Updating the mysql tables

--- admin/sql/install.sql
+++ admin/sql/install.sql
@@ -1,29 +1,38 @@
-DROP TABLE IF EXISTS `#__mtwmultiple_sites`;
+--
+-- Table structure for table `jos_mtwmultiple_sites`
+--
+
 CREATE TABLE IF NOT EXISTS `jos_mtwmultiple_sites` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `title` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `email` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created_by` int(11) NOT NULL,
   `created` datetime NOT NULL,
-  `password` varchar(255) collate utf8_unicode_ci NOT NULL,
-  PRIMARY KEY  (`id`)
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `checked_out` datetime NOT NULL,
+  `checked_out_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
-DROP TABLE IF EXISTS `#__mtwmultiple_extensions`;
+--
+-- Table structure for table `jos_mtwmultiple_extensions`
+--
+
 CREATE TABLE IF NOT EXISTS `jos_mtwmultiple_extensions` (
-  `id` int(11) NOT NULL auto_increment,
-  `filename` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `type` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `name` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `author` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `creationDate` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `copyright` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `license` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `authorEmail` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `authorUrl` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `version` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `enable` int(11) NOT NULL default '1',
-  PRIMARY KEY  (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `author` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `creationDate` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `copyright` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `license` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `authorEmail` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `authorUrl` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `enable` int(11) NOT NULL DEFAULT '1',
+  `checked_out` int(11) NOT NULL,
+  `checked_out_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-
