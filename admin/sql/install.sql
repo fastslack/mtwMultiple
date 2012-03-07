71bb1fe2d738c924db5dd8afd2a29cbac24ce806
Matias Aguirre <maguirre@matware.com.ar>
2012-03-07 04:26:38 -0300

    Universalize (?) the prefix

--- admin/sql/install.sql
+++ admin/sql/install.sql
@@ -1,8 +1,8 @@
 --
--- Table structure for table `jos_mtwmultiple_sites`
+-- Table structure for table `#__mtwmultiple_sites`
 --
 
-CREATE TABLE IF NOT EXISTS `jos_mtwmultiple_sites` (
+CREATE TABLE IF NOT EXISTS `#__mtwmultiple_sites` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -16,10 +16,10 @@ CREATE TABLE IF NOT EXISTS `jos_mtwmultiple_sites` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
--- Table structure for table `jos_mtwmultiple_extensions`
+-- Table structure for table `#__mtwmultiple_extensions`
 --
 
-CREATE TABLE IF NOT EXISTS `jos_mtwmultiple_extensions` (
+CREATE TABLE IF NOT EXISTS `#__mtwmultiple_extensions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
